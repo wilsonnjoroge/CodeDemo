c5ab63ae964f5eac04ac327f8ca5b7e2bbcc9f79
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-10-12 09:21:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const PORT = 4500; // define the port
+const PORT = 4500; 
 
 const app = express();
 
-// simple get request
 app.get('/demo', (req, res) => {
   res.send("This is a demo application")
 })
